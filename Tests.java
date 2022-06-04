535e34c923a7b671bc684b4729dbf38f0cd2bffa
Boss-of-Trees <87606790+Boss-of-Trees@users.noreply.github.com>
2022-06-04 14:44:45 +0200

    Test Datei

---
+++ Tests.java
@@ -0,0 +1,12 @@
+import Backend.OrdnerSpeicherer;
+import Backend.OrdnerStruktur;
+import Backend.Stapel;
+
+public class Tests {
+
+    Stapel st = new Stapel();
+    OrdnerStruktur os = new OrdnerStruktur();
+
+    OrdnerSpeicherer osp = new OrdnerSpeicherer();
+
+}

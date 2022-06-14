1bb070902f3f0b72c89afd91f82c3cc966ba57ce
Boss-of-Trees <87606790+Boss-of-Trees@users.noreply.github.com>
2022-06-14 11:46:32 +0200

    ExternalLibraries als .jar hinzugefügt

--- ExternalLibraries/org/sqlite/SQLiteCommitListener.java
+++
@@ -1,9 +0,0 @@
-package ExternalLibraries.org.sqlite;
-
-/** https://www.sqlite.org/c3ref/commit_hook.html */
-public interface SQLiteCommitListener {
-
-    void onCommit();
-
-    void onRollback();
-}
--- Tests.java
+++ Tests.java
@@ -1,16 +1,18 @@
 import java.sql.*;
-import ExternalLibraries.org.sqlite.*;
+import org.sqlite.*;
 
 public class Tests {
     public static void main(String[] args) {
         Connection c = null;
 
         try {
-            Class.forName("ExternalLibraries.org.sqlite.JDBC");
+            Class.forName("org.sqlite.JDBC");
             c = DriverManager.getConnection("jdbc:sqlite:test.db");
         } catch ( Exception e ) {
+            /**
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-            System.exit(0);
+            System.exit(0);*/
+            e.printStackTrace();
         }
         System.out.println("Opened database successfully");
     }

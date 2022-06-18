84dd3b6c028311b7041636d7015d6f76b2d0ee69
Tryptician <106529367+Tryptician@users.noreply.github.com>
2022-06-18 13:32:55 +0200

    Update DatenElement.java

--- Backend/DatenElement.java
+++ Backend/DatenElement.java
@@ -2,5 +2,54 @@ package Backend;
 
 import java.io.Serializable;
 
-public class DatenElement implements Serializable {
+public class DatenElement implements Serializable {     
+  
+     private String r;
+     private String v;
+     private int level;
+   
+    public DatenElement(String ruckseite, String vorderseite)
+    {
+       r = ruckseite;
+       v= vorderseite;
+       level = 0;
+    }
+
+     // Parametern ruckseite und vorderseite
+    public String rgeben()
+    {
+      return r;
+    }
+     public String vgeben()
+    {
+      return v;
+    }
+     public void rsetzen(String ruck)
+    {
+        r = ruck;
+    }
+    public void vsetzen(String vorder)
+    {
+        v = vorder;
+    }
+    public int lgeben()
+    {
+        return level;
+    }
+    public void levelsteigen()
+    {
+        level = level + 1;
+    }
+    public void levelsinken()
+    {
+        if(level == 0)
+        {
+            level = 0;
+        }
+        else
+        {
+            level = level - 1;
+        }
+    }
 }
+

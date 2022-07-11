9092aaa60e2e6690cc32ee2190d1664264a9cd85
Kannyones <106530590+Kannyones@users.noreply.github.com>
2022-07-11 12:43:55 +0200

    Update Referenz.java
    hab hier nichts gemacht

--- Backend/Referenz.java
+++ Backend/Referenz.java
@@ -1,4 +1,50 @@
-package Backend;
+import java.util.ArrayList;
 
-public class Referenz implements OrdnerElement{
+public class Referenz implements OrdnerElement {
+
+    Ordner vorgaenger;
+    String name;
+    String pfad;
+
+    public Referenz(String name, Ordner vorgaenger){
+        this.name = name;
+        this.vorgaenger = vorgaenger;
+        pfad = "";
+    }
+
+    public void einfuegen(OrdnerElement neuesElement, Ordner vorgaenger) {
+        System.out.println("Why you calling this on a reference?");
+    }
+    
+    public void vorgaengerSetzten(Ordner ordner) {
+        vorgaenger = ordner;
+    }
+
+    public void ordnerEntfernen(String name) {
+        System.out.println("Why you calling this on a reference?");
+    }
+
+    public void refernzEntfernen(String name) {
+        System.out.println("Why you calling this on a reference?");
+    }
+
+    public Ordner ordnerGeben(String name) {
+        return vorgaenger;
+    }
+
+    public Stapel stapelGeben(String name) {
+        return null;
+    }
+
+    public ArrayList<String[]> ordnerStrukturAlsStringGeben() {
+        return null;
+    }
+
+    public Ordner vogaengerGeben() {
+        return vorgaenger;
+    }
+
+    public String nameGeben() {
+        return name;
+    }
 }

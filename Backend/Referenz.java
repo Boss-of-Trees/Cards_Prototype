477c8b14c4494ef030a2963c643a2cf2b79e509c
Kannyones <106530590+Kannyones@users.noreply.github.com>
2022-07-12 12:55:13 +0200

    Update Referenz.java

--- Backend/Referenz.java
+++ Backend/Referenz.java
@@ -1,5 +1,3 @@
-package Backend;
-
 import java.util.ArrayList;
 
 public class Referenz implements OrdnerElement {
@@ -8,45 +6,66 @@ public class Referenz implements OrdnerElement {
     String name;
     String pfad;
 
-    public Referenz(String name, Ordner vorgaenger){
+    public Referenz(String name, Ordner vorgaenger)
+    {
         this.name = name;
         this.vorgaenger = vorgaenger;
         pfad = "";
     }
 
-    public void einfuegen(OrdnerElement neuesElement, Ordner vorgaenger) {
+    
+    public void einfuegen(OrdnerElement neuesElement, Ordner vorgaenger)
+    {
         System.out.println("Why you calling this on a reference?");
     }
     
-    public void vorgaengerSetzten(Ordner ordner) {
+    
+    public void vorgaengerSetzten(Ordner ordner) 
+    {
         vorgaenger = ordner;
     }
 
-    public void ordnerEntfernen(String name) {
+    
+    public void ordnerEntfernen(String name) 
+    {
         System.out.println("Why you calling this on a reference?");
     }
 
-    public void refernzEntfernen(String name) {
+    
+    public void refernzEntfernen(String name) 
+    {
         System.out.println("Why you calling this on a reference?");
     }
 
-    public Ordner ordnerGeben(String name) {
+    
+    public Ordner ordnerGeben(String name) 
+    {
         return vorgaenger;
     }
 
-    public Stapel stapelGeben(String name) {
+    
+    public Stapel stapelGeben(String name) 
+    {
         return null;
     }
 
-    public ArrayList<String[]> ordnerStrukturAlsStringGeben() {
+    
+    public ArrayList<String[]> ordnerStrukturAlsStringGeben()
+    {
         return null;
     }
 
-    public Ordner vogaengerGeben() {
+    
+    public Ordner vogaengerGeben() 
+    {
         return vorgaenger;
     }
 
-    public String nameGeben() {
+    
+    public String nameGeben() 
+    {
         return name;
     }
+    
+    
 }

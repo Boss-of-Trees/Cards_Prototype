83690d24291ac54f004593af2c40c60fb805fa8a
Kannyones <106530590+Kannyones@users.noreply.github.com>
2022-07-12 12:53:57 +0200

    Update Ordner.java

--- Backend/Ordner.java
+++ Backend/Ordner.java
@@ -1,5 +1,3 @@
-package Backend;
-
 import java.util.ArrayList;
 
 public class Ordner implements OrdnerElement{
@@ -11,89 +9,141 @@ public class Ordner implements OrdnerElement{
     ArrayList<Referenz> referenzen;
     ArrayList<String> referenzenNamen;
 
-    public Ordner(String name, Ordner vorgaenger){
+    public Ordner(String name, Ordner vorgaenger)
+    {
         this.name = name;
-        if(vorgaenger == null){
+        if(vorgaenger == null)
+        {
             this.vorgaenger = this;
         } else {
             this.vorgaenger = vorgaenger;
         }
+        
     }
 
-    public void einfuegen(OrdnerElement neuesElement, Ordner vorgaenger) {
-        if(neuesElement.getClass().getName() == "Referenz"){
-            if(!referenzenNamen.contains(neuesElement.nameGeben())){
+    
+    public void einfuegen(OrdnerElement neuesElement, Ordner vorgaenger)
+    {
+        
+        if(neuesElement.getClass().getName() == "Referenz")
+        {
+            
+            if(!referenzenNamen.contains(neuesElement.nameGeben()))
+            {
                 neuesElement.vorgaengerSetzten(this);
                 referenzen.add((Referenz) neuesElement);
                 referenzenNamen.add(neuesElement.nameGeben());
             }
-        } else {
-            if(!unterordnerNamen.contains(neuesElement.nameGeben())){
+            
+        } else 
+        {
+            
+            if(!unterordnerNamen.contains(neuesElement.nameGeben()))
+            {
                 neuesElement.vorgaengerSetzten(this);
                 unterordner.add((Ordner) neuesElement);
                 unterordnerNamen.add(neuesElement.nameGeben());
             }
+            
         }
+        
     }
     
-    public void vorgaengerSetzten(Ordner ordner) {
+    
+    public void vorgaengerSetzten(Ordner ordner)
+    {
         vorgaenger = ordner;
     }
      
-    public void ordnerEntfernen(String name) {
+    
+    public void ordnerEntfernen(String name)
+    {
+        
         if (unterordnerNamen.contains(name))
         {
-            for(int i = 0; i < unterordnerNamen.size(); i++){
-                if(unterordnerNamen.get(i).equals(name)){
+            
+            for(int i = 0; i < unterordnerNamen.size(); i++)
+            {
+                
+                if(unterordnerNamen.get(i).equals(name))
+                {
                     unterordnerNamen.remove(i);
                     unterordner.remove(i);
                 }
+                
             }
+            
         }
+        
     }
 
-    public void refernzEntfernen(String name) {
+    
+    public void refernzEntfernen(String name) 
+    {
+        
         if(referenzenNamen.contains(name))
         {
-            for(int i = 0; i < referenzenNamen.size(); i++){
+            
+            for(int i = 0; i < referenzenNamen.size(); i++)
+            {
+                
                 if(referenzenNamen.get(i).equals(name))
                 {
                     referenzenNamen.remove(i);
                     referenzen.remove(i);
                 }
-                }
+                
             }
+                
+            }
+            
         }
         
-    public Ordner ordnerGeben(String name){
+        
+    public Ordner ordnerGeben(String name)
+    {
+        
         if(unterordnerNamen.contains(name))
         {
+            
             for(int i = 0; i < unterordnerNamen.size(); i++)
             {
+                
                 if(unterordnerNamen.get(i).equals(name))
                 {
                     return unterordner.get(i); 
                 }
+                
                 }
+                
             }
         return null;
             }
     
-    public Stapel stapelGeben(String name) {
+            
+    public Stapel stapelGeben(String name) 
+    {
+        
         if(referenzenNamen.contains(name))
         {
+            
             for(int i = 0; i < referenzenNamen.size(); i++)
             {
+                
                 if(referenzenNamen.get(i).equals(name))
                 {
                     return referenzen.get(i).stapelGeben(name); 
                 }
+                
                 }
+                
             }
         return null;
     }
 
-    public ArrayList<String[]> ordnerStrukturAlsStringGeben() {
+    
+    public ArrayList<String[]> ordnerStrukturAlsStringGeben()
+    {
         ArrayList<String[]> toBeReturned = new ArrayList<>();
         for (int i = 0 ; i <unterordnerNamen.size(); i++)
         {
@@ -102,6 +152,7 @@ public class Ordner implements OrdnerElement{
             asString[1] = unterordnerNamen.get(i);
             toBeReturned.add(asString);
         }
+        
         for (int i = 0 ; i <referenzenNamen.size(); i++)
         {
             String[] asString = new String[2];
@@ -117,8 +168,10 @@ public class Ordner implements OrdnerElement{
         return vorgaenger;
     }
 
+    
     public String nameGeben() {
         return name;
     }
 
+    
 }

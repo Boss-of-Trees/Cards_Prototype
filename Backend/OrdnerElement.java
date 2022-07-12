0e8662eb72dadec43eb972ec43c08d5e9031a373
Kannyones <106530590+Kannyones@users.noreply.github.com>
2022-07-12 12:54:25 +0200

    Update OrdnerElement.java

--- Backend/OrdnerElement.java
+++ Backend/OrdnerElement.java
@@ -1,16 +1,23 @@
-package Backend;
-
 import java.util.ArrayList;
 
 public interface OrdnerElement {
 
     public abstract void einfuegen(OrdnerElement neuesElement, Ordner vorgaenger);
+    
     public abstract void ordnerEntfernen(String name);
-    public abstract void refernzEntfernen(String name);
+    
     public abstract Ordner ordnerGeben(String name);
+    
     public abstract Stapel stapelGeben(String name);
+    
     public abstract ArrayList<String[]> ordnerStrukturAlsStringGeben();
+    
+    public abstract void refernzEntfernen(String name);
+    
     public abstract Ordner vogaengerGeben();
+    
     public abstract String nameGeben();
+    
     public abstract void vorgaengerSetzten(Ordner ordner);
+    
 }

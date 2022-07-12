42a0731ef9013c237621dfe4a03aa42d84b6e907
Kannyones <106530590+Kannyones@users.noreply.github.com>
2022-07-12 20:50:27 +0200

    Update OrdnerStruktur.java

--- Backend/OrdnerStruktur.java
+++ Backend/OrdnerStruktur.java
@@ -1,12 +1,102 @@
-package Backend;
-
 import java.io.Serializable;
+import java.util.ArrayList;
 
-public class OrdnerStruktur implements Serializable {
+public class OrdnerStruktur implements Serializable 
+{
 
-    //bitte ändern damit es richtig funktioniert
-    public String nameGeben(){
-        return null;
+    private Ordner root;
+    private Ordner aktiverOrdner;
+    private String name;
+
+    public OrdnerStruktur(String benutzerName)
+    {
+        root = new Ordner("root_" + benutzerName, null);
+        name = benutzerName;
+        aktiverOrdner = root;
     }
+    
 
+    public void einfuegen(OrdnerElement neuesElement)
+    {
+        aktiverOrdnerZuRoot();
+        aktiverOrdner.einfuegen(neuesElement, aktiverOrdner.vogaengerGeben());
+    }
+    
+
+    public void ordnerEntfernen(String name)
+    {
+        aktiverOrdnerZuRoot();
+        aktiverOrdner.ordnerEntfernen(name);
+    }
+    
+
+    public void referenzEntfernen(String name)
+    {
+        aktiverOrdnerZuRoot();
+        aktiverOrdner.refernzEntfernen(name);
+    }
+    
+
+    public Ordner ordnerGeben(String name)
+    {
+        aktiverOrdnerZuRoot();
+        if(name.equals(aktiverOrdner.nameGeben()))
+        {
+            return aktiverOrdner;
+        }
+        return aktiverOrdner.ordnerGeben(name);
+    }
+    
+
+    public Ordner aktiverOrdnerGeben()
+    {
+        aktiverOrdnerZuRoot();
+        return aktiverOrdner;
+    }
+    
+
+    public Stapel stapelGeben(String name)
+    {
+        aktiverOrdnerZuRoot();
+        return aktiverOrdner.stapelGeben(name);
+    }
+    
+
+    public ArrayList<String[]> ordnerStrukturAlsStringArrayListGeben()
+    {
+        aktiverOrdnerZuRoot();
+        return aktiverOrdner.ordnerStrukturAlsStringGeben();
+    }
+    
+
+    public void ordnerWechselnRunter(String name)
+    {
+        aktiverOrdnerZuRoot();
+        aktiverOrdner = aktiverOrdner.ordnerGeben(name);
+    }
+    
+
+    public void ordnerWechselnHoch()
+    {
+        aktiverOrdnerZuRoot();
+        aktiverOrdner = aktiverOrdner.vogaengerGeben();
+    }
+    
+
+    public String nameGeben()
+    {
+        return name;
+    }
+    
+
+    private void aktiverOrdnerZuRoot()
+    {
+        
+        if(aktiverOrdner == null)
+        {
+            aktiverOrdner = root;
+        }
+        
+    }
+    
 }

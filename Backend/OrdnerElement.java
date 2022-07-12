1c6141621a5d31b34dcb77d6bd108601a18cb5e4
Kannyones <106530590+Kannyones@users.noreply.github.com>
2022-07-12 20:51:58 +0200

    Update OrdnerElement.java

--- Backend/OrdnerElement.java
+++ Backend/OrdnerElement.java
@@ -6,18 +6,18 @@ public interface OrdnerElement {
     
     public abstract void ordnerEntfernen(String name);
     
+    public abstract void refernzEntfernen(String name);
+    
     public abstract Ordner ordnerGeben(String name);
     
     public abstract Stapel stapelGeben(String name);
     
     public abstract ArrayList<String[]> ordnerStrukturAlsStringGeben();
     
-    public abstract void refernzEntfernen(String name);
-    
     public abstract Ordner vogaengerGeben();
     
     public abstract String nameGeben();
-    
+
     public abstract void vorgaengerSetzten(Ordner ordner);
-    
+
 }

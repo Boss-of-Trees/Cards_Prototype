e8ba4c10dbb4a0d120e405ad33d5c123fdea1ce1
Kannyones <106530590+Kannyones@users.noreply.github.com>
2022-07-12 20:51:32 +0200

    Update Referenz.java

--- Backend/Referenz.java
+++ Backend/Referenz.java
@@ -1,71 +1,83 @@
+import java.io.File;
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Referenz implements OrdnerElement {
+public class Referenz implements OrdnerElement, Serializable {
 
     Ordner vorgaenger;
     String name;
     String pfad;
 
-    public Referenz(String name, Ordner vorgaenger)
+    public Referenz(String name, String pfad, Ordner vorgaenger, Stapel stapel)
     {
         this.name = name;
         this.vorgaenger = vorgaenger;
-        pfad = "";
-    }
+        this.pfad = pfad + name;
+        StapelSpeicherer stsp = new StapelSpeicherer();
+        stsp.speichern(pfad, stapel);
 
+    }
     
+
     public void einfuegen(OrdnerElement neuesElement, Ordner vorgaenger)
     {
         System.out.println("Why you calling this on a reference?");
     }
     
-    
-    public void vorgaengerSetzten(Ordner ordner) 
-    {
-        vorgaenger = ordner;
-    }
 
-    
     public void ordnerEntfernen(String name) 
     {
         System.out.println("Why you calling this on a reference?");
     }
-
     
+
     public void refernzEntfernen(String name) 
     {
         System.out.println("Why you calling this on a reference?");
     }
-
     
+
     public Ordner ordnerGeben(String name) 
     {
         return vorgaenger;
     }
-
     
-    public Stapel stapelGeben(String name) 
+
+    public Stapel stapelGeben(String name)
     {
-        return null;
+        StapelSpeicherer stapelSpeicherer = new StapelSpeicherer();
+        return stapelSpeicherer.laden(pfad);
     }
-
     
+
     public ArrayList<String[]> ordnerStrukturAlsStringGeben()
     {
         return null;
     }
-
     
+
     public Ordner vogaengerGeben() 
     {
         return vorgaenger;
     }
-
     
-    public String nameGeben() 
+
+    public String nameGeben()
     {
         return name;
     }
     
+
+    public void vorgaengerSetzten(Ordner ordner)
+    {
+        vorgaenger = ordner;
+    }
     
+
+    public void entfernen()
+    {
+        File file = new File(pfad + ".stapel");
+        file.delete();
+    }
+
 }
